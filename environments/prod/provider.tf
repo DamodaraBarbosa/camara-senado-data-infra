c57e409feeb4abc381e4a1266f6b93cc4e52bf2d
Damodara Barbosa <damodarabarbosa@gmail.com>
2026-03-16 21:43:58 -0300

    feat: configuring the provider for localstack aws

--- environments/prod/provider.tf
+++ environments/prod/provider.tf
@@ -1,13 +1,24 @@
 terraform {
-    required_providers {
-        databricks = {
-            source  = "databricks/databricks"
-            version = "~> 1.0"
-        } 
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
+  }
 }
 
-provider "databricks" {
-    host  = var.databricks_host
-    token = var.databricks_token
+provider "aws" {
+  access_key                  = "test"
+  secret_key                  = "test"
+  region                      = "us-east-1"
+  s3_use_path_style           = true
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+
+  endpoints {
+    s3  = "http://localhost:4566"
+    iam = "http://localhost:4566"
+    glue = "http://localhost:4566"
+  }
 }
