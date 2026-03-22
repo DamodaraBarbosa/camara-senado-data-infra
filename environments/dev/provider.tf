06f43e6294635cc56bfc393690f0d041d0f058ec
Damodara Barbosa <damodarabarbosa@gmail.com>
2026-03-22 20:39:00 -0300

    feat: config glues in main and backend settings

--- environments/dev/provider.tf
+++ environments/dev/provider.tf
@@ -5,6 +5,13 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  backend "s3" {
+    bucket         = "dataplatform-terraform-state-damodarabarbosa-dev"
+    key            = "dev/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-state-lock"
+  }
 }
 
 provider "aws" {
